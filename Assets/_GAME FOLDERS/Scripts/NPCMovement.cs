854bc3846448793996ef9535d131d3005469af39
Burak ÖZGEN <35861357+Burakzgen@users.noreply.github.com>
2024-05-20 01:46:01 +0300

    up

---
+++ Assets/_GAME FOLDERS/Scripts/NPCMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float waypointTolerance = 0.1f;
+    [SerializeField] private Animator animator;
+
+    private int currentWaypointIndex = 0;
+
+    private void Start()
+    {
+        if (waypoints.Count > 0)
+        {
+            StartCoroutine(MoveToNextWaypoint());
+        }
+        else
+        {
+            Debug.LogWarning("Waypoints list is empty!");
+        }
+    }
+
+    private IEnumerator MoveToNextWaypoint()
+    {
+        while (currentWaypointIndex < waypoints.Count)
+        {
+            Transform targetWaypoint = waypoints[currentWaypointIndex];
+
+            while (Vector3.Distance(transform.position, targetWaypoint.position) > waypointTolerance)
+            {
+                Vector3 direction = (targetWaypoint.position - transform.position).normalized;
+                transform.position += direction * moveSpeed * Time.deltaTime;
+
+                transform.LookAt(targetWaypoint.position);
+
+                yield return null;
+            }
+
+            currentWaypointIndex++;
+        }
+        if (animator != null)
+        {
+            animator.Play("Falling");
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    if (waypoints != null)
+    //    {
+    //        for (int i = 0; i < waypoints.Count; i++)
+    //        {
+    //            Gizmos.color = Color.red;
+    //            Gizmos.DrawSphere(waypoints[i].position, 0.3f);
+
+    //            if (i < waypoints.Count - 1)
+    //            {
+    //                Gizmos.color = Color.yellow;
+    //                Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
+    //            }
+    //        }
+    //    }
+    //}
+}
