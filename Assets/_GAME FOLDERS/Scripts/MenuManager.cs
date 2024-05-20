28097eefd025a8008d379107277daeae29ce7897
Burak ÖZGEN <35861357+Burakzgen@users.noreply.github.com>
2024-05-20 19:50:43 +0300

    up

--- Assets/_GAME FOLDERS/Scripts/MenuManager.cs
+++ Assets/_GAME FOLDERS/Scripts/MenuManager.cs
@@ -4,14 +4,21 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    #region VARIABLES
+    // Genel butonlar 
     [SerializeField] Button _startButton, _showTeamButton, _hideTeamButton, _hideStartButton, _quitButton, _showOptionsButton, _hideOptionsButton;
-    bool _isActive = false;
     [SerializeField] GameObject _teamPanel, _warningPanel, _loadingPanel, _startPanel, _optionsPanel, _modePanel, _typePanel;
+    bool _isActive = false;
     AudioSource _audioSource;
+    // Oyun modu
     [SerializeField] Button _easyMode, _hardMode;
     [SerializeField] Button _womanType, _manType;
+    // Ayarlar menu
     [SerializeField] Slider _volumeSlider;
     [SerializeField] Button _dynamicCamera, _staticCamera;
+    #endregion
+
+    #region  UNITY
     private void Awake()
     {
         if (!PlayerPrefs.HasKey("Coefficient"))
@@ -21,7 +28,7 @@ public class MenuManager : MonoBehaviour
         if (!PlayerPrefs.HasKey("CharacterType"))
             PlayerPrefs.SetString("CharacterType", "Man");
         if (!PlayerPrefs.HasKey("CameraMode"))
-            PlayerPrefs.SetInt("CameraMode", 0);
+            PlayerPrefs.SetInt("CameraMode", 1);
     }
     private void Start()
     {
@@ -30,6 +37,15 @@ public class MenuManager : MonoBehaviour
         AudioListener.volume = volume;
 
         _audioSource = GetComponent<AudioSource>();
+        AddListeners();
+
+    }
+    private void OnDestroy()
+    {
+        RemoveListeners();
+    }
+    void AddListeners()
+    {
         _startButton.onClick.AddListener(StartGame);
         _hideStartButton.onClick.AddListener(HideStartPanel);
         _showTeamButton.onClick.AddListener(ShowTeamPanel);
@@ -46,19 +62,29 @@ public class MenuManager : MonoBehaviour
 
         _dynamicCamera.onClick.AddListener(() => SelectedCameraMode(0));
         _staticCamera.onClick.AddListener(() => SelectedCameraMode(1));
-
     }
-
-    void StartGame()
+    void RemoveListeners()
     {
-        _startPanel.SetActive(true);
-        PanelEffect(_startPanel, 1, 1f, 0.3f, Ease.InCirc);
+        _startButton.onClick.RemoveListener(StartGame);
+        _hideStartButton.onClick.RemoveListener(HideStartPanel);
+        _showTeamButton.onClick.RemoveListener(ShowTeamPanel);
+        _hideTeamButton.onClick.RemoveListener(HideTeamPanel);
+        _easyMode.onClick.RemoveListener(() => ModeSelected("Easy"));
+        _hardMode.onClick.RemoveListener(() => ModeSelected("Hard"));
 
+        _womanType.onClick.RemoveListener(() => CharacterTypeSelected("Woman"));
+        _manType.onClick.RemoveListener(() => CharacterTypeSelected("Man"));
+        _quitButton.onClick.RemoveListener(QuitGame);
+
+        _showOptionsButton.onClick.RemoveListener(ShowOptionsPanel);
+        _hideOptionsButton.onClick.RemoveListener(HideOptionsPanel);
+
+        _dynamicCamera.onClick.RemoveListener(() => SelectedCameraMode(0));
+        _staticCamera.onClick.RemoveListener(() => SelectedCameraMode(1));
     }
-    void HideStartPanel()
-    {
-        PanelEffect(_startPanel, 1, 0.2f, 0.2f, Ease.OutCirc, () => _startPanel.SetActive(false));
-    }
+    #endregion
+
+    #region TEAM WINDOW
     void ShowTeamPanel()
     {
         _teamPanel.SetActive(true);
@@ -66,14 +92,14 @@ public class MenuManager : MonoBehaviour
     }
     void HideTeamPanel() =>
         PanelEffect(_teamPanel, 0, 0.2f, 0.2f, Ease.OutCirc, () => _teamPanel.SetActive(false));
+    #endregion
 
-
+    #region OPTIONS WINDOW
     void ShowOptionsPanel()
     {
         _optionsPanel.SetActive(true);
         _volumeSlider.value = PlayerPrefs.GetFloat("Volume");
         PanelEffect(_optionsPanel, 1, 1f, 0.3f, Ease.InCirc);
-
     }
     void HideOptionsPanel()
     {
@@ -95,6 +121,9 @@ public class MenuManager : MonoBehaviour
 
         HideOptionsPanel();
     }
+    #endregion
+
+    #region UTILITY
     public void ShowHidePanel()
     {
         _isActive = !_isActive;
@@ -115,9 +144,21 @@ public class MenuManager : MonoBehaviour
             tweenCallback?.Invoke();
         });
     }
+    #endregion
 
+    #region START WINDOW
+    void StartGame()
+    {
+        _startPanel.SetActive(true);
+        PanelEffect(_startPanel, 1, 1f, 0.3f, Ease.InCirc);
+    }
+    void HideStartPanel()
+    {
+        PanelEffect(_startPanel, 1, 0.2f, 0.2f, Ease.OutCirc, () => _startPanel.SetActive(false));
+    }
     void ModeSelected(string mode)
     {
+        _audioSource.Play();
         if (mode == "Easy")
             PlayerPrefs.SetFloat("Coefficient", 1f);
         else
@@ -131,6 +172,7 @@ public class MenuManager : MonoBehaviour
     }
     void CharacterTypeSelected(string mode)
     {
+        _audioSource.Play();
         if (mode == "Woman")
             PlayerPrefs.SetString("CharacterType", "Woman");
         else
@@ -139,4 +181,5 @@ public class MenuManager : MonoBehaviour
 
         _loadingPanel.GetComponent<Image>().DOFade(1f, 1f).SetEase(Ease.InQuart).OnComplete(() => UnityEngine.SceneManagement.SceneManager.LoadScene(1));
     }
+    #endregion
 }
